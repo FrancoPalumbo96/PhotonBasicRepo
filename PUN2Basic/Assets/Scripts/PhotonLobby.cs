76d13960e4dbaa96597d9ff95d50fefe9cb13718
Franco Palumbo <palumbofranco9a@gmail.com>
2019-12-11 11:51:08 -0300

    First commit

---
+++ PUN2Basic/Assets/Scripts/PhotonLobby.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+
+    //Button that makes the player search for a room
+    public GameObject battleButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        lobby = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Connects to Master photon server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player jas connected to the Photon master server");
+        //All the clients will load to the same scene as the master
+        PhotonNetwork.AutomaticallySyncScene = true;
+        battleButton.SetActive(true);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Faied to join a Random Room, No Rooms available");
+        CreateRoom();
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create Room, Room already existe");
+        CreateRoom();
+    }
+
+
+    private void CreateRoom()
+    {
+        Debug.Log("Room was created");
+        int randomRoomName = Random.Range(0, 100);
+        RoomOptions roomOptions = new RoomOptions() {IsVisible = true, IsOpen = true, MaxPlayers = 10};
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOptions);
+
+    }
+
+    public void OnBattlerButtonClicked()
+    {
+        Debug.Log("Batlle button clicked");
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        Debug.Log("Cancel button clicked");
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PUN2Basic/Assets/Scripts/PhotonRoom.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhotonRoom : MonoBehaviourPunCallbacks, IInRoomCallbacks
+{
+
+    public static PhotonRoom room;
+    private PhotonView PV;
+
+    public int multiplayerScene;
+
+    public int currentScene;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        //setup singelton
+        if (PhotonRoom.room == null)
+        {
+            PhotonRoom.room = this;
+        }
+        else
+        {
+            if (PhotonRoom.room != this)
+            {
+                Destroy(PhotonRoom.room.gameObject);
+                PhotonRoom.room = this;
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+        PV = GetComponent<PhotonView>();
+    }
+
+    
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        PhotonNetwork.AddCallbackTarget(this);
+        SceneManager.sceneLoaded += OnSceneFinishLoading;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        PhotonNetwork.RemoveCallbackTarget(this);
+        SceneManager.sceneLoaded -= OnSceneFinishLoading;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Has joined room");
+        if(!PhotonNetwork.IsMasterClient) return;
+        StartGame();
+
+    }
+
+    private void StartGame()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+        PhotonNetwork.LoadLevel(multiplayerScene);
+    }
+
+    private void OnSceneFinishLoading(Scene scene, LoadSceneMode mode)
+    {
+        currentScene = scene.buildIndex;
+        if (currentScene == multiplayerScene)
+        {
+            CreatePlayer();
+        }
+    }
+
+    private void CreatePlayer()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonNetworkPlayer"), transform.position,
+            Quaternion.identity, 0);
+    }
+
+}
